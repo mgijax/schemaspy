c091dfcf97e4a5b8e6a555f71320d45e163887c0
MGI Admin <mgiadmin@jax.org>
2013-06-25 14:58:30 +0000

    TR11382/scrum-dog

--- schemaspySybase.sh
+++ schemaspySybase.sh
@@ -11,6 +11,6 @@ port=`grep "MGD_DBURL=" ${MGICONFIG}/master.config.sh | cut -d":" -f2 | sed "s/\
 rm -rf ${DATALOADSOUTPUT}/mgi/schemaspy-$host
 mkdir ${DATALOADSOUTPUT}/mgi/schemaspy-$host
 
-echo buildSybaseDocs $host $port ${MGD_DBNAME} ${MGD_DBSERVER} ${DATALOADSOUTPUT}/mgi/schemaspy-$host
+echo buildSybaseDocs $host $port ${MGD_DBSERVER} ${MGD_DBNAME} ${MGD_DBUSER} `cat ${MGD_DBPASSWORDFILE}` ${DATALOADSOUTPUT}/mgi/schemaspy-$host
 ./buildSybaseDocs $host $port ${MGD_DBSERVER} ${MGD_DBNAME} ${MGD_DBUSER} `cat ${MGD_DBPASSWORDFILE}` ${DATALOADSOUTPUT}/mgi/schemaspy-$host
 
